b1927cc9680103e51c9d114a18427d49c26d461f
fengluo <fengluo17@gmail.com>
2016-01-01 22:14:07 +0800

    compatible with Mac

--- Package.swift
+++ Package.swift
@@ -1,6 +1,12 @@
 import PackageDescription
 
+#if os(OSX)
+    let CHiredisURL = "https://github.com/fengluo/CHiredis-OSX.git"
+#else
+    let CHiredisURL = "https://github.com/fengluo/CHiredis.git"
+#endif
+
 let package = Package(
     dependencies: [
-        .Package(url: "https://github.com/fengluo/CHiredis", majorVersion: 0, minor: 2)    ]
+        .Package(url: CHiredisURL, majorVersion: 0, minor: 2)]
 )
